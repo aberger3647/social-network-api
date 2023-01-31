909d82b42df5915576510acc5c1062f6019f268e
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2023-01-31 17:43:22 -0600

    add update user route, add delete uesr route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,23 @@
 const { ObjectId } = require('mongoose').Types;
 const { User } = require('../models');
 
+const userCount = async() =>
+    User.aggregate()
+    .count('userCount')
+    .then((numberOfUsers) => numberOfUsers)
+
 // routes for /api/users/
 module.exports = {
 // get all users
     getUsers(req, res) {
         User.find()
-        .then(async (users) => res.json(users))
+        .then(async (users) => {
+            const userObj = {
+                users,
+                userCount: await userCount()
+            };
+            return res.json(userObj);
+        })
          .catch((err) => {
         console.log(err);
         return res.status(500).json(err);
@@ -31,11 +42,23 @@ module.exports = {
         User.create(req.body)
         .then((user) => res.json(user))
         .catch((err) => res.status(500).json(err));
-    }
+    },
 // put to update a user by _id
-
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { new: true }
+        )
+        .then(updatedUser => res.status(200).json(updatedUser))
+        .catch(err => res.status(500).json(err))
+    },
 // delete to remove by _id
-
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+        .then((user) => res.json(user))
+        .catch((err) => res.status(500).json(err));
+    }
 // bonus: remove user's associated thoughts when deleted
 
 // routes for /api/users/:userId/friends/:friendId
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -14,8 +14,7 @@ const {
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser)
-// .put(updateUser).delete(deleteUser);
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
 // router.router('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
