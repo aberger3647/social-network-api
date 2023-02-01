e60b74331544413496ef8098e88c59a937f54e99
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2023-02-01 16:55:11 -0600

    delete friend

--- controllers/userController.js
+++ controllers/userController.js
@@ -78,7 +78,11 @@ module.exports = {
     },
 // delete to remove friend from friend list
     deleteFriend(req, res) {
-        User.findOneAndUpdate({ _id: req.params.userId })
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { new: true }
+            )
         .then((user) => {
             !user
             ? res.status(404).json({ message: 'No user found with that ID' })
